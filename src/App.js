45e67300aaa48a4156da107dec7f473022d52aae
Stephen Selao <stephenselaodev@gmail.com>
2021-01-17 14:33:05 -0500

    Add variable click values

--- src/App.js
+++ src/App.js
@@ -7,35 +7,40 @@ class App extends Component {
   // FIXME: Temporary leaving it in App
   state = {
     currency: 1,
+    currencyPerClick: 1,
     currencyPerSecond: 1000,
     upgrades: [
       {
         name: 'Upgrade 1',
+        type: 'click',
         initialCost: 1,
         cost: 1,
         count: 0,
-        cpsIncrease: 1,
+        increase: 1,
       },
       {
         name: 'Upgrade 2',
+        type: 'generator',
         initialCost: 2,
         cost: 2,
         count: 0,
-        cpsIncrease: 1,
+        increase: 1,
       },
       {
         name: 'Upgrade 3',
+        type: 'generator',
         initialCost: 5,
         cost: 5,
         count: 0,
-        cpsIncrease: 5,
+        increase: 5,
       },
       {
         name: 'Upgrade 4',
+        type: 'generator',
         initialCost: 10,
         cost: 10,
         count: 0,
-        cpsIncrease: 10,
+        increase: 10,
         disabled: true,
       },
     ],
@@ -48,7 +53,7 @@ class App extends Component {
   }
 
   clickHandler = () => {
-    this.setState((state) => ({ currency: state.currency + 1 }));
+    this.setState((state) => ({ currency: state.currency + state.currencyPerClick }));
   };
 
   clickedUpgradeHandler = (index) => {
@@ -65,11 +70,18 @@ class App extends Component {
           ),
         };
 
-        return {
+        const newState = {
           currency: state.currency - cost,
-          currencyPerSecond: state.currencyPerSecond + updatedUpgrades[index].cpsIncrease,
-          upgrades: updatedUpgrades,
+          upgrades: updatedUpgrades
         };
+
+        if (updatedUpgrades[index].type === 'click') {
+          newState.currencyPerClick = state.currencyPerClick + updatedUpgrades[index].increase;
+        } else if (updatedUpgrades[index].type === 'generator') {
+          newState.currencyPerSecond = state.currencyPerSecond + updatedUpgrades[index].increase;
+        }
+
+        return newState;
       }
     });
   };
@@ -80,6 +92,7 @@ class App extends Component {
         <div id='leftSection'>
           <Clicker
             currency={this.state.currency}
+            currencyPerClick={this.state.currencyPerClick}
             currencyPerSecond={this.state.currencyPerSecond}
             clicked={this.clickHandler}
           />
--- src/components/Clicker/Clicker.js
+++ src/components/Clicker/Clicker.js
@@ -10,6 +10,7 @@ class Clicker extends Component {
       <React.Fragment>
         <img src={image} className={styles.Clicker} onClick={this.props.clicked} alt='clicker' />
         <div className={styles.ClickerNumberDisplay}>{this.props.currency} makis</div>
+        <div>{this.props.currencyPerClick} makis per click</div>
         <ClickGenerator currencyPerSecond={this.props.currencyPerSecond} />
       </React.Fragment>
     );
