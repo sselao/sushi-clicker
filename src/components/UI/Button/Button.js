388663043cd3cdafd20cb9ff04e0dca28565c3af
Stephen Selao <stephenselaodev@gmail.com>
2021-01-17 12:31:16 -0500

    Fix styling

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import styles from './Button.module.css';
 
-const Button = (props) => <button onClick={props.clicked}>{props.children}</button>;
+const Button = (props) => (
+  <button className={styles.Button} onClick={props.clicked}>
+    {props.children}
+  </button>
+);
 
 export default Button;
