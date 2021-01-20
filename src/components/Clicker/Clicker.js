8c8d68c102404de9e58024651d41548da07ad1fe
Stephen Selao <stephenselaodev@gmail.com>
2021-01-20 17:19:45 -0500

    Refactor return statements

--- src/components/Clicker/Clicker.js
+++ src/components/Clicker/Clicker.js
@@ -1,23 +1,20 @@
 import React from 'react';
-
 import image from './clicker.svg';
 import styles from './Clicker.module.css';
 import ClickGenerator from '../ClickGenerator/ClickGenerator';
 import Currency from '../UI/Currency/Currency';
 
-const Clicker = (props) => {
-  return (
-    <>
-      <img src={image} className={styles.Clicker} onClick={props.clicked} alt='clicker' />
-      <div className={styles.ClickerNumberDisplay}>
-        <Currency value={props.currency} />
-      </div>
-      <div>
-        <Currency value={props.currencyPerClick} /> per click
-      </div>
-      <ClickGenerator currencyPerSecond={props.currencyPerSecond} />
-    </>
-  );
-};
+const Clicker = (props) => (
+  <>
+    <img src={image} className={styles.Clicker} onClick={props.clicked} alt='clicker' />
+    <div className={styles.ClickerNumberDisplay}>
+      <Currency value={props.currency} />
+    </div>
+    <div>
+      <Currency value={props.currencyPerClick} /> per click
+    </div>
+    <ClickGenerator currencyPerSecond={props.currencyPerSecond} />
+  </>
+);
 
 export default Clicker;
--- src/components/PowerUpsList/PowerUpsList.js
+++ src/components/PowerUpsList/PowerUpsList.js
@@ -2,25 +2,23 @@ import React from 'react';
 import PowerUp from './PowerUp/PowerUp';
 import styles from './PowerUpsList.module.css';
 
-const PowerUpsList = (props) => {
-  return (
-    <div className={styles.PowerUpsList}>
-      <h1>List of Power Ups</h1>
-      <div className={styles.List}>
-        {props.powerUps.map((pu, index) => (
-          <PowerUp
-            key={index}
-            name={pu.name}
-            type={pu.type}
-            cost={pu.cost}
-            multiplier={pu.multiplier}
-            enabled={pu.enabled}
-            clicked={() => props.clicked(index)}
-          />
-        ))}
-      </div>
+const PowerUpsList = (props) => (
+  <div className={styles.PowerUpsList}>
+    <h1>List of Power Ups</h1>
+    <div className={styles.List}>
+      {props.powerUps.map((pu, index) => (
+        <PowerUp
+          key={index}
+          name={pu.name}
+          type={pu.type}
+          cost={pu.cost}
+          multiplier={pu.multiplier}
+          enabled={pu.enabled}
+          clicked={() => props.clicked(index)}
+        />
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default PowerUpsList;
