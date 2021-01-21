39fb3379e2390b4c53a962e2227357543e22e268
Stephen Selao <stephenselaodev@gmail.com>
2021-01-21 11:53:41 -0500

    Remove div.App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Layout from './containers/Layout/Layout';
 
-const App = () => (
-  <div className='App'>
-    <Layout />
-  </div>
-);
+const App = () => <Layout />;
 
 export default App;
--- src/containers/Upgrades/Upgrades.js
+++ src/containers/Upgrades/Upgrades.js
@@ -2,10 +2,10 @@ import React from 'react';
 import UpgradesList from '../../components/UpgradesList/UpgradesList';
 
 const Upgrades = (props) => (
-  <div>
+  <>
     <h1>List of Upgrades</h1>
     <UpgradesList upgrades={props.upgrades} upgraded={props.upgraded} />
-  </div>
+  </>
 );
 
 export default Upgrades;
