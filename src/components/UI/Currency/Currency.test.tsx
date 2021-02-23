60a25c4f0d7b2bbac722ac5c014e004db73337b5
Stephen Selao <stephenselaodev@gmail.com>
2021-02-23 14:57:32 -0500

    Add singular form test for currency

--- src/components/UI/Currency/Currency.test.tsx
+++ src/components/UI/Currency/Currency.test.tsx
@@ -1,9 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
 import Currency from './Currency';
 
 describe('Currency', () => {
+  test('renders singular form', () => {
+    const { baseElement } = render(<Currency value={1} />);
+    expect(within(baseElement as HTMLElement).getByText('1 nigiri')).toBeTruthy();
+  });
+
   test('renders plural form', () => {
-    const { getByText } = render(<Currency value={999} />);
-    expect(getByText('999 nigiris')).toBeInTheDocument();
+    const { baseElement } = render(<Currency value={99} />);
+    expect(within(baseElement as HTMLElement).getByText('99 nigiris')).toBeTruthy();
   });
 });
